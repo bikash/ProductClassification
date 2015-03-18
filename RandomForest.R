7e4f7bb07f92f7cfde9e3265c01daf5a91208b1b
Bikash <er.bikash21@gmail.com>
2015-03-18 14:30:48 +0100

    Random Forest

---
+++ RandomForest.R
@@ -0,0 +1,14 @@
+#################################################################################################
+#################################################################################################
+## Author: Bikash Agrawal
+## Date: 12th March Feb 2015
+## Email: er.bikash21@gmail.com
+## Description: Random Forest to classify product in Otto Group Kaggle Product classification competition.
+##     http://www.kaggle.com/c/otto-group-product-classification-challenge
+##
+## References: 
+## [1] http://dnene.bitbucket.org/docs/mlclass-notes/lecture16.html
+## [2] 
+## 
+#################################################################################################
+#################################################################################################
