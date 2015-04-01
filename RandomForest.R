d1a1dc0c31b2cec0659898c690e2816d491cadd7
Bikash <er.bikash21@gmail.com>
2015-04-01 12:18:07 +0200

    Ctree modifition in control

--- RandomForest.R
+++ RandomForest.R
@@ -46,14 +46,14 @@ head(train) ## column name -> id, feat_1,.......feat_93, target
 # pred <- predict.boosting(fit,newdata=test1)
 # pred$class
 # pred$error
-
-pdf("graph/missmap.pdf",bg="white")
-## Display missing data from training data sets
-missmap(train1, main="Training Data - Missings Map", col=c("green", "red"), legend=FALSE)
-dev.off()
+# 
+# pdf("graph/missmap.pdf",bg="white")
+# ## Display missing data from training data sets
+# missmap(train1, main="Training Data - Missings Map", col=c("green", "red"), legend=FALSE)
+# dev.off()
 
 # remove id column so it doesn't get picked up by the random forest classifier
-train2 <- train1[,-1]
+train2 <- train[,-1]
 
 ## decision treee
 # model <- rpart(target ~ ., method = "class", data = train2)
@@ -69,53 +69,61 @@ train2 <- train1[,-1]
 #summary(train1$id)
 
 train2 <- train[,-1]
-# set a unique seed number so you get the same results everytime you run the below model,
-# the number does not matter
-set.seed(12)
-fit <- randomForest(as.factor(target) ~ ., data=train2, ntree=500, method = "class")
 
-pdf("graph/varimpPlot.pdf",bg="white")
-# Look at variable importance
-varImpPlot(fit)
-dev.off()
-# use the random forest model to create a prediction
-pred <- predict(fit,test,type="prob")
-submit <- data.frame(id = test$id, pred)
-write.csv(submit, file = "output/submit_1.csv", row.names = FALSE)
+me.randomforest <- function(train2,test)
+{
 
+  # set a unique seed number so you get the same results everytime you run the below model,
+  # the number does not matter
+  set.seed(12)
+  fit <- randomForest(as.factor(target) ~ ., data=train2, ntree=500, method = "class")
+  
+  pdf("graph/varimpPlot.pdf",bg="white")
+  # Look at variable importance
+  varImpPlot(fit)
+  dev.off()
+  # use the random forest model to create a prediction
+  pred <- predict(fit,test,type="prob")
+  submit <- data.frame(id = test$id, pred)
+  write.csv(submit, file = "output/submit_1.csv", row.names = FALSE)
+}
 
 
 ### Condition Inference Random Forest
-library(party)
-print("Prediction using Condition Inference Random Forest......")
-c.fit <- cforest(as.factor(target) ~ .,
-               data = train2, controls=cforest_unbiased(ntree=50, mtry=3))
+me.crandomForest<-function(train2, test)
+{
+    library(party)
+    print("Prediction using Condition Inference Random Forest......")
+    c.fit <- cforest(target ~ .,data = train2)
+    
+    # ## Tree structure
+    # c.fit = ctree(as.factor(target) ~.,
+    #             data = train2,   
+    #             controls = ctree_control(
+    #               teststat="quad",
+    #               testtype="Univariate",
+    #               mincriterion=.95,
+    #               minsplit=10, 
+    #               minbucket=5,
+    #               maxdepth=0
+    #             ))
+    # prp(c.fit)
+    # fancyRpartPlot(c.fit)
+    
+    #prediction
+    Prediction <- predict(c.fit, test, OOB=TRUE, type = "response")
+    #prop.table(table(test$target, Prediction),1)
+    ## calculate accuracy of model
+    #accuracy = sum(Prediction==test$target)/length(Prediction)
+    #print (sprintf("Accuracy = %3.2f %%",accuracy*100)) ### 82.84% accuracy of model
+    #########################################################################
+    out <- data.frame(id = test$id, Prediction)
+    write.csv(out, file = "output/ciRandomForest-predict1.csv", row.names = FALSE)
+    #########################################################################
+    #########################################################################
+}
 
-# ## Tree structure
-# c.fit = ctree(as.factor(target) ~.,
-#             data = train2,   
-#             controls = ctree_control(
-#               teststat="quad",
-#               testtype="Univariate",
-#               mincriterion=.95,
-#               minsplit=10, 
-#               minbucket=5,
-#               maxdepth=0
-#             ))
-# prp(c.fit)
-# fancyRpartPlot(c.fit)
-
-#prediction
-Prediction <- predict(c.fit, test, OOB=TRUE, type = "response")
-#prop.table(table(test$target, Prediction),1)
-## calculate accuracy of model
-#accuracy = sum(Prediction==test$target)/length(Prediction)
-#print (sprintf("Accuracy = %3.2f %%",accuracy*100)) ### 82.84% accuracy of model
-#########################################################################
-out <- data.frame(id = test$id, Prediction)
-write.csv(out, file = "output/ciRandomForest-predict1.csv", row.names = FALSE)
-#########################################################################
-#########################################################################
+me.crandomForest(train2, test)
 
 ##SVM
 
