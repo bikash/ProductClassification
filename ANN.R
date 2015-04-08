a902712c08a62ec42388d0c9c45c2fadc517e431
Bikash <er.bikash21@gmail.com>
2015-04-08 21:39:51 +0200

    Neural network

--- ANN.R
+++ ANN.R
@@ -1,12 +1,37 @@
+#################################################################################################
+#################################################################################################
+## Author: Bikash Agrawal
+## Date: 12th March Feb 2015
+## Email: er.bikash21@gmail.com
+## Description: Neural Network to classify product in Otto Group Kaggle Product classification competition.
+##     http://www.kaggle.com/c/otto-group-product-classification-challenge
+##
+## References: 
+## [1] http://dnene.bitbucket.org/docs/mlclass-notes/lecture16.html
+## [2] 
+## 
+#################################################################################################
+#################################################################################################
+
 #Load dependency
 library(nnet)
 
 # set seed
 set.seed(1337)
 
+### setting path of repo folder.
+getwd()
+setwd("/Users/bikash/repos/kaggle/ProductClassification/")
+
 #load data
-train<-read.csv("Data/train.csv")
-test<-read.csv("Data/test.csv")
+##########################################################################
+########Cleaning up training dataset #####################################
+##########################################################################
+print("Data Cleaning up process......")
+train <- read.csv("data/train.csv", header=TRUE)
+test <- read.csv("data/test.csv", header=TRUE)
+##########################################################################
+
 
 # fit and predict
 fit<-nnet(target ~ ., train[,-1], size = 3, rang = 0.1, decay = 5e-4, maxit = 500)
@@ -14,4 +39,6 @@ predicted<-as.data.frame(predict(fit,test[,-1],type="raw"))
 
 id<-test[,1]
 output<-cbind(id,predicted)
-write.csv(output,"Output/neural_network_1.csv",row.names=FALSE)
+
+
+write.csv(output,"output/neural_network_1.csv",row.names=FALSE)
